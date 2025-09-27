195c0e30335dfd9f4c1e5a0b1664f354fbcd24a2
navod99 <navx99@gmail.com>
2025-09-27 19:20:33 +0530

    Add Swagger documentation

--- blog-backend/src/app.controller.ts
+++ blog-backend/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Check if the API is running' })
+  @ApiResponse({ status: 200, description: 'API is running' })
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('status')
+  @ApiOperation({ summary: 'Get database status' })
+  @ApiResponse({ status: 200, description: 'Database is connected' })
+  getStatus(): { status: string; database: string } {
+    return this.appService.getDatabaseStatus();
   }
 }
--- blog-backend/src/app.service.ts
+++ blog-backend/src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to the Blog Platform API!';
+  }
+
+  getDatabaseStatus(): { status: string; database: string } {
+    const dbStatus =
+      Number(this.connection.readyState) === 1 ? 'connected' : 'disconnected';
+    return {
+      status: 'API is running',
+      database: `MongoDB ${dbStatus}`,
+    };
   }
 }
